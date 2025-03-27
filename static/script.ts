c8c9e8d8876ece16c9023ebf673e88c119f90984
david-espindola01 <david.espindola01@uptc.edu.co>
2025-03-26 23:22:59 -0500

    Update script.ts

--- static/script.ts
+++ static/script.ts
@@ -1,12 +1,15 @@
+const API_URL = "https://distributed-webapp-production.up.railway.app/ping";
+
 const button = document.getElementById("pingButton") as HTMLButtonElement;
 const result = document.getElementById("result") as HTMLDivElement;
 
 button.addEventListener("click", async () => {
     try {
-        const response = await fetch("http://localhost:5000/ping");
+        const response = await fetch(API_URL);
         const data = await response.json();
         result.innerText = data.message;
     } catch (error) {
         result.innerText = "Error al conectar con el backend";
     }
 });
+
