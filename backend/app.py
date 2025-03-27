c027cd5c3ccaa7429c5d10ecd4ff5ccae9a8726f
david-espindola01 <david.espindola01@uptc.edu.co>
2025-03-27 00:02:48 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import time
 
 app = Flask(__name__)
-CORS(app)  # Permitir conexión desde otras máquinas
+CORS(app)
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({"message": "Pong"}), 200
+    start_time = time.time()  # Captura el tiempo de inicio
+    response = jsonify({"message": "Pong", "ping": round((time.time() - start_time) * 1000, 2)})  # Calcula el ping en ms
+    return response, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
